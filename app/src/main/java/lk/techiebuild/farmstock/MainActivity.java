3c2e2d586576120ab82ac247945ab52d4e97233c
Dinuka <drpiyadigama@gmail.com>
2019-10-13 17:05:28 +0530

    Display text from FireStore field onClick

--- app/src/main/java/lk/techiebuild/farmstock/MainActivity.java
+++ app/src/main/java/lk/techiebuild/farmstock/MainActivity.java
@@ -1,14 +1,64 @@
 package lk.techiebuild.farmstock;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Access a Cloud Firestore instance from your Activity
+    DocumentReference mDocRef = FirebaseFirestore.getInstance().document("itemsInStore/IPhIJekwM4cpHz1YLYP7");
+
+//    private static final String TAG = "DocSnippets";
+
+    TextView mQuoteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mQuoteTextView = findViewById(R.id.textViewContent);
+    }
+
+    public void fetchContent(View view){
+//        db.collection("itemsInStore")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()){
+                    String quoteText = documentSnapshot.getString("Item Name");
+
+                    mQuoteTextView.setText(quoteText);
+                }
+            }
+        });
+
     }
 }
